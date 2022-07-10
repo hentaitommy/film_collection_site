d8f0ebb56467f6286565f1337a879bd03fd5c443
tommyjqliu <hentaitommy@qq.com>
2022-07-10 16:21:22 +0800

    test

---
+++ nextjs_part/pages/api/film/[filmId]/comment/index.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		let prisma: PrismaClient;
+		switch (req.method) {
+			case 'GET':
+				const filmId = +(req.query.filmId as string)
+				console.log(req.query.filmId)
+				prisma = new PrismaClient()
+				const comments = await prisma.comment.findMany({
+					where: {
+						filmId
+					}
+				})
+				res.status(200).json(comments);
+				await prisma.$disconnect()
+				break
+
+			case 'POST':
+				prisma = new PrismaClient()
+				const comment = await prisma.comment.create({
+					data: {
+						filmId: +(req.query.filmId as string),
+						username: req.body.username,
+						content: req.body.content,
+					}
+				})
+				res.status(200).json(comment);
+				await prisma.$disconnect()
+				break
+
+			default:
+				res.status(405).json({ error: 'Method not allowed' })
+		}
+	} catch (error: any) {
+		console.log(error)
+		res.status(500).json(error);
+	}
+}
