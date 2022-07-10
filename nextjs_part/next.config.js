47babac5ceaa75e42e665863720e61dd996c95cd
tommyjqliu <hentaitommy@qq.com>
2022-07-10 21:22:05 +0800

    feat: 用户侧首页

--- nextjs_part/next.config.js
+++ nextjs_part/next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+let doubanDomains = []
+for (let i = 1; i < 10; i++) {
+  doubanDomains.push(`img${i}.doubanio.com`)
+}
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['img9.doubanio.com'],
+    domains: [...doubanDomains],
   },
 }
+
 
 module.exports = nextConfig
